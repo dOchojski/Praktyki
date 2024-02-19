93e40061c21fb145fa55220bbb2a380abdd609fd
H-4-P-P-Y <jakub.freier@gmail.com>
2024-02-19 10:46:14 +0100

    Typy Danych

--- src/main/java/com/d_d/praktyki/PraktykiApplication.java
+++ src/main/java/com/d_d/praktyki/PraktykiApplication.java
@@ -8,6 +8,17 @@ public class PraktykiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PraktykiApplication.class, args);
-    }
 
+        String brand;
+        String model;
+        int age;
+        double price;
+
+        brand = "BWM";
+        model = "M3";
+        age = 2020;
+        price = 199599.99;
+
+        System.out.println("Marka: " + brand + " Model: " + model + " Rocznik: " + age + " Cena: " + price);
+    }
 }
