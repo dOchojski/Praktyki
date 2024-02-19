d355c5337030cbc461129b2ea215cc7ce1725cdd
H-4-P-P-Y <jakub.freier@gmail.com>
2024-02-19 13:34:32 +0100

    Agregacja

--- src/main/java/com/d_d/praktyki/PraktykiApplication.java
+++ src/main/java/com/d_d/praktyki/PraktykiApplication.java
@@ -3,13 +3,33 @@ package com.d_d.praktyki;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+class Uzytkownik{
+
+    String imie;
+    String nazwisko;
+    String adresZamieszkania;
+    String adresZameldowania;
+}
+
 @SpringBootApplication
 public class PraktykiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PraktykiApplication.class, args);
 
-        KontoBankowe konto1 = new KontoBankowe();
+        Uzytkownik adam = new Uzytkownik();
+        adam.imie = "Adam";
+        adam.nazwisko = "Kowalski";
+        adam.adresZamieszkania = "ul. Piłsudzkiego 10";
+        adam.adresZameldowania = "ul. Piłsudzkiego 10";
+
+        Uzytkownik ewa = new Uzytkownik();
+        adam.imie = "Ewa";
+        adam.nazwisko = "Nowak";
+        adam.adresZamieszkania = "al. Legionów Polskich 28";
+        adam.adresZameldowania = "ul. Mariackiej 47";
+
+        KontoBankowe konto1 = new KontoBankowe(adam);
         konto1.stanKonta = 56000;
         konto1.numerKonta = "PL49 1020 2892 2276 3005 0000 0000";
         konto1.wplacPieniadze(2.50);
@@ -17,7 +37,7 @@ public class PraktykiApplication {
         konto1.wplacPieniadze(500);
         konto1.wyplacPieniadze(199.99);
 
-        KontoBankowe konto2 = new KontoBankowe();
+        KontoBankowe konto2 = new KontoBankowe(ewa);
         konto2.stanKonta = 13000;
         konto2.numerKonta = "PL49 1020 3898 2276 3006 0000 0000";
         konto2.wplacPieniadze(5000);
@@ -29,8 +49,12 @@ public class PraktykiApplication {
     }
 }
 class KontoBankowe{
+    public KontoBankowe(Uzytkownik uzytkownik){
+        wlasciciel = uzytkownik;
+    }
     float stanKonta;
     String numerKonta;
+    Uzytkownik wlasciciel;
     public void wplacPieniadze(double kwota)
     {
         stanKonta += kwota;
