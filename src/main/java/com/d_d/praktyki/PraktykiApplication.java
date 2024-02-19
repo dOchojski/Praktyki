4c756ff7a23e4334be4d7424695353c86b86d909
Holmynik <dominikkijko4@gmail.com>
2024-02-19 12:49:38 +0100

    Typy Danych

--- src/main/java/com/d_d/praktyki/PraktykiApplication.java
+++ src/main/java/com/d_d/praktyki/PraktykiApplication.java
@@ -8,6 +8,22 @@ public class PraktykiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PraktykiApplication.class, args);
+
+        String brand;
+        String model;
+        int age;
+        double price;
+
+        brand="Fiat";
+        model = "600e";
+        age=2023;
+        price=170000.00;
+
+        System.out.println("Marka: "+brand);
+        System.out.println("Model: "+model);
+        System.out.println("Rocznik: "+age);
+        System.out.println("Cena: "+price);
+
     }
 
 }
