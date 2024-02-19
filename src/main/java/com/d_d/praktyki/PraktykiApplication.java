c2f9c64417928e1a70a7eafbe0163c9c25c02570
Sebastian <sebasqbi@gmail.com>
2024-02-19 09:46:40 +0100

    Typy Danych

--- src/main/java/com/d_d/praktyki/PraktykiApplication.java
+++ src/main/java/com/d_d/praktyki/PraktykiApplication.java
@@ -8,6 +8,15 @@ public class PraktykiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PraktykiApplication.class, args);
+        String marka = "Fiat";
+        String model = "Seicento";
+        int rocznik = 2005;
+        double cena = 2500; // zł
+
+        System.out.println(marka);
+        System.out.println(model);
+        System.out.println(rocznik);
+        System.out.println(cena);
     }
 
 }
