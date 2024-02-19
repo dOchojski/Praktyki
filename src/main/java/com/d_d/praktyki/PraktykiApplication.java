f2e1fe723b581b09682c47d7035b39f794986fc7
Sebastian <sebasqbi@gmail.com>
2024-02-19 12:08:30 +0100

    Agregacja

--- src/main/java/com/d_d/praktyki/PraktykiApplication.java
+++ src/main/java/com/d_d/praktyki/PraktykiApplication.java
@@ -3,9 +3,27 @@ package com.d_d.praktyki;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Random;
+
+class Osoba {
+    Osoba(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+
+    public String fullname() {
+        return imie+" "+nazwisko;
+    }
+
+    private String imie;
+    private String nazwisko;
+}
+
 class KontoBankowe {
-    KontoBankowe(String numer) {
-        numerKonta = numer;
+    KontoBankowe(Osoba daneOsobowe) {
+        Random rand = new Random();
+        wlasciciel = daneOsobowe;
+        numerKonta = String.valueOf(rand.nextInt(10000));
         stanKonta = 0;
     }
 
@@ -22,11 +40,12 @@ class KontoBankowe {
     }
 
     void wyswietlSaldo() {
-        System.out.println(String.format("Saldo konta %s: %f", numerKonta, stanKonta));
+        System.out.println(String.format("Saldo konta %s (%s): %f", numerKonta, wlasciciel.fullname(), stanKonta));
     }
 
     private double stanKonta;
     private String numerKonta;
+    private Osoba wlasciciel;
 }
 
 @SpringBootApplication
@@ -35,8 +54,11 @@ public class PraktykiApplication {
     public static void main(String[] args) {
         SpringApplication.run(PraktykiApplication.class, args);
 
-        KontoBankowe a = new KontoBankowe("111");
-        KontoBankowe b = new KontoBankowe("222");
+        Osoba adam = new Osoba("Adam", "Kowalski");
+        Osoba andrzej = new Osoba("Andrzej", "Nowak");
+
+        KontoBankowe a = new KontoBankowe(adam);
+        KontoBankowe b = new KontoBankowe(andrzej);
         try {
             a.wplacPieniadze(5);
             b.wplacPieniadze(2);
