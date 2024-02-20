04ae9efddc5465b24d70d6c0638797b60324e516
Sebastian <sebasqbi@gmail.com>
2024-02-20 10:15:51 +0100

    Add mark test

--- src/main/java/com/d_d/praktyki/averagemark/MarkTest.java
+++ src/main/java/com/d_d/praktyki/averagemark/MarkTest.java
@@ -2,6 +2,9 @@ package com.d_d.praktyki.averagemark;
 
 public class MarkTest {
     public static void main(String[] args) {
-
+        AverageMark avg = new AverageMark();
+        avg.readMarks(5);
+        avg.showMarks();
+        System.out.println(avg.calculate());
     }
 }
