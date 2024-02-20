e22945e83d76c02ad912877123bba6ddb5513fc4
Sebastian <sebasqbi@gmail.com>
2024-02-20 10:55:22 +0100

    Add PasswordCheck class

--- src/main/java/com/d_d/praktyki/passwordcheck/PasswordCheck.java
+++ src/main/java/com/d_d/praktyki/passwordcheck/PasswordCheck.java
@@ -1,4 +1,24 @@
 package com.d_d.praktyki.passwordcheck;
 
+import java.util.regex.Pattern;
+
 public class PasswordCheck {
+    private Pattern passwordPattern;
+    public PasswordCheck() {
+        // check if password:
+        // - contains letters
+        // - contains digits
+        // and match if token has at least 8 chars
+        final String regex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
+        setPattern(regex);
+    }
+    public PasswordCheck(final String regex) {
+        setPattern(regex);
+    }
+    public boolean isValid(String password) {
+        return passwordPattern.matcher(password).matches();
+    }
+    private void setPattern(final String regex) {
+        passwordPattern = Pattern.compile(regex);
+    }
 }
--- src/main/java/com/d_d/praktyki/passwordcheck/PasswordTest.java
+++ src/main/java/com/d_d/praktyki/passwordcheck/PasswordTest.java
@@ -9,15 +9,14 @@ public class PasswordTest {
         // - contains letters
         // - contains digits
         // and match if token has at least 8 chars
-        String regex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
-        Pattern pattern = Pattern.compile(regex);
+        PasswordCheck checker = new PasswordCheck();
         final List passwords = List.of("A", "BCE", "Baklanaz", "Baklazan123", "bak23", "1231241241412tttt");
         passwords.forEach(password -> {
             System.out.println(
                 String.format(
                     "%s - %s",
                     password,
-                    pattern.matcher((String) password).matches() ? "is valid" : "is invalid"
+                    checker.isValid((String) password) ? "is valid" : "is invalid"
                 )
             );
         });
