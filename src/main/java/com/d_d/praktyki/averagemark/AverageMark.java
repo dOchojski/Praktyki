6a12ca13278d1eb3178a70aea5758d5aeab1af7c
Sebastian <sebasqbi@gmail.com>
2024-02-20 09:49:57 +0100

    Add average mark

---
+++ src/main/java/com/d_d/praktyki/averagemark/AverageMark.java
@@ -0,0 +1,4 @@
+package com.d_d.praktyki.averagemark;
+
+public class AverageMark {
+}
---
+++ src/main/java/com/d_d/praktyki/averagemark/MarkTest.java
@@ -0,0 +1,7 @@
+package com.d_d.praktyki.averagemark;
+
+public class MarkTest {
+    public static void main(String[] args) {
+
+    }
+}
