315608f0f138d09805470d4465dd3fd0e8e4331d
Sebastian <sebasqbi@gmail.com>
2024-02-20 09:04:04 +0100

    Add calculator tests

--- src/main/java/com/d_d/praktyki/calculator/Calculator.java
+++ src/main/java/com/d_d/praktyki/calculator/Calculator.java
@@ -1,6 +1,8 @@
 package com.d_d.praktyki.calculator;
 
 public class Calculator {
+    // Disable default constructor
+    private Calculator() {}
     public static double add(double a, double b) { return add(a,b,0); }
     public static double add(double a, double b, double c) { return a+b+c; }
     public static double subtract(double a, double b) { return subtract(a,b,0); }
