1ae80aaf303e4e1f02a81bc8704984907c2e344c
Sebastian <sebasqbi@gmail.com>
2024-02-20 11:18:57 +0100

    Add GuessNumber class

--- src/main/java/com/d_d/praktyki/guessnumber/GuessNumber.java
+++ src/main/java/com/d_d/praktyki/guessnumber/GuessNumber.java
@@ -1,5 +1,29 @@
 package com.d_d.praktyki.guessnumber;
 
-public class GuessNumber {
+import java.util.Random;
+import java.util.Scanner;
 
+public class GuessNumber {
+    private int guessingNumber = rand.nextInt(100)+1;
+    private static Random rand = new Random();
+    private Scanner reader = new Scanner(System.in);
+    public boolean prompt() {
+        System.out.print("Wpisz liczbę (1-100)> ");
+        int input = reader.nextInt();
+        return handle(input);
+    }
+    private boolean handle(int input) {
+        if (input == guessingNumber) {
+            System.out.println("Liczba została odgadnięta!");
+            return true;
+        }
+        else if (input < guessingNumber) {
+            System.out.println("Za mało!");
+            return false;
+        }
+        else /*(input > guessingNumber)*/ {
+            System.out.println("Za dużo!");
+            return false;
+        }
+    }
 }
--- src/main/java/com/d_d/praktyki/guessnumber/GuessTest.java
+++ src/main/java/com/d_d/praktyki/guessnumber/GuessTest.java
@@ -4,23 +4,12 @@ import java.util.Scanner;
 
 public class GuessTest {
     public static void main(String[] args) {
-        int guessingNumber = 70;
+        GuessNumber guess = new GuessNumber();
 
-        Scanner reader = new Scanner(System.in);
         System.out.println("=== Zgadnij liczbę ===");
-        while (true /*number was not guessed*/) {
-            System.out.print("Wpisz liczbę (1-100)> ");
-            int input = reader.nextInt();
-            if (input == guessingNumber) {
-                System.out.println("Liczba została odgadnięta!");
-                break;
-            }
-            else if (input < guessingNumber) {
-                System.out.println("Za mało!");
-            }
-            else if (input > guessingNumber) {
-                System.out.println("Za dużo!");
-            }
+        boolean has_win = false;
+        while (!has_win /*number was not guessed*/) {
+            has_win = guess.prompt();
         }
     }
 }
