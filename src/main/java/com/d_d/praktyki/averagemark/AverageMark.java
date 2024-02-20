f05ae53057e32830ea8f7aeb521c45b834af41c3
Sebastian <sebasqbi@gmail.com>
2024-02-20 10:15:37 +0100

    Add AverageMark class

--- src/main/java/com/d_d/praktyki/averagemark/AverageMark.java
+++ src/main/java/com/d_d/praktyki/averagemark/AverageMark.java
@@ -1,4 +1,34 @@
 package com.d_d.praktyki.averagemark;
 
+import java.util.Scanner;
+import java.util.Vector;
+
 public class AverageMark {
+    private static Scanner reader = new Scanner(System.in);
+    private Vector<Integer> marks = new Vector<>();
+
+    /**
+     * Reads marks from standard input stream.
+     * @param n the number of marks to be read.
+     */
+    public void readMarks(int n) {
+        for (int i=0; i<n; i++)
+            marks.add(reader.nextInt());
+    }
+
+    public void showMarks() {
+        marks.forEach(mark -> {
+            System.out.println(mark);
+        });
+    }
+
+    /**
+     * Calculate average of marks.
+     * @return average of input marks.
+     */
+    public double calculate() {
+        var sum = marks.stream().mapToInt(Integer::valueOf).sum();
+        var count = marks.stream().count();
+        return sum / count;
+    }
 }
