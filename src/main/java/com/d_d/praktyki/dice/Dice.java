f92c1646204ef9137d86ed92bfb749c265e7dbf0
Sebastian <sebasqbi@gmail.com>
2024-02-20 09:39:10 +0100

    Add dice class

--- src/main/java/com/d_d/praktyki/dice/Dice.java
+++ src/main/java/com/d_d/praktyki/dice/Dice.java
@@ -1,5 +1,28 @@
 package com.d_d.praktyki.dice;
 
-public class Dice {
+import java.util.Random;
 
+public class Dice {
+    private int sides;
+    private Random rand = new Random();
+    public Dice() { sides = 6; }
+    public Dice(int sides) { this.sides = sides; }
+
+    /**
+     * Roll single die.
+     * @return random value from range [1, sides+1)
+     */
+    public int roll() {
+        return rand.nextInt(sides)+1;
+    }
+    /**
+     * Roll multiple dice.
+     * @param rolls the number of dice to be rolled
+     * @return sum of random values each from range [1, sides+1)
+     */
+    public int roll(int rolls) {
+        return rand.ints(1, sides+1)
+                .limit(rolls)
+                .sum();
+    }
 }
