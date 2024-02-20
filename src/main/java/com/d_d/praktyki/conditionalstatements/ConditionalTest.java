d1cc65465c9a72f929510b570cc930ec921e95a0
Sebastian <sebasqbi@gmail.com>
2024-02-20 11:56:40 +0100

    Add ValueCheck class

--- src/main/java/com/d_d/praktyki/conditionalstatements/ConditionalTest.java
+++ src/main/java/com/d_d/praktyki/conditionalstatements/ConditionalTest.java
@@ -2,6 +2,7 @@ package com.d_d.praktyki.conditionalstatements;
 
 public class ConditionalTest {
     public static void main(String[] args) {
-        
+        ValueCheck check = new ValueCheck();
+        System.out.println("Wartość: "+check.getFormattedValue());
     }
 }
