d46507ddb5185296b0408b2faf72dc5ac53931f8
Sebastian <sebasqbi@gmail.com>
2024-02-20 10:21:15 +0100

    Add Password Check

---
+++ src/main/java/com/d_d/praktyki/passwordcheck/PasswordCheck.java
@@ -0,0 +1,4 @@
+package com.d_d.praktyki.passwordcheck;
+
+public class PasswordCheck {
+}
---
+++ src/main/java/com/d_d/praktyki/passwordcheck/PasswordTest.java
@@ -0,0 +1,7 @@
+package com.d_d.praktyki.passwordcheck;
+
+public class PasswordTest {
+    public static void main(String[] args) {
+        
+    }
+}
