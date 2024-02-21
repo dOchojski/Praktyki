edf67007d5cf5200793c87a29c46367549c2b4f3
Holmynik <dominikkijko4@gmail.com>
2024-02-21 12:02:42 +0100

    PasswordCheck

---
+++ src/main/java/com/d_d/praktyki/PasswordCheck/PasswordTest.java
@@ -0,0 +1,31 @@
+package com.d_d.praktyki.PasswordCheck;
+
+import java.util.List;
+import java.util.regex.Pattern;
+public class PasswordTest {
+    public static void main(String[] args) {
+
+        final String reg = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\dd]{8,}$";
+        Pattern pat = Pattern.compile(reg);
+        PasswordCheck check = new PasswordCheck();
+        List hasla = List.of("H2dwswj", "Kot", "7737772", "98HhJjkLK","KOTKI282","kotki3323");
+        hasla.forEach(password -> {
+            if (pat.matcher((String) password).matches()) System.out.println(
+                    String.format(
+                            "%s - %s",
+                            password,
+                            "is valid",
+                            check.isValid((String) password) ? "is valid" : "is invalid"
+                    )
+            );
+            else System.out.println(
+                    String.format(
+                            "%s - %s",
+                            password,
+                            "is invalid",
+                            check.isValid((String) password) ? "is valid" : "is invalid"
+                    )
+            );
+        });
+    }
+}
