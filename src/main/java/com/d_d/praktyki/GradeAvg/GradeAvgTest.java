176cb605625b83f8316db5581292b4293ddea945
Holmynik <dominikkijko4@gmail.com>
2024-02-21 09:55:30 +0100

    AverageMark

--- src/main/java/com/d_d/praktyki/GradeAvg/GradeAvgTest.java
+++ src/main/java/com/d_d/praktyki/GradeAvg/GradeAvgTest.java
@@ -1,2 +1,12 @@
-package com.d_d.praktyki.GradeAvg;public class GradeAvgTest {
+package com.d_d.praktyki.GradeAvg;
+
+public class GradeAvgTest {
+    public static void main(String[] args) {
+
+        AverageMark nr1 = new AverageMark();
+
+        nr1.readMarks(5);
+        nr1.showMarks();
+        System.out.println(nr1.calculate());
+    }
 }
