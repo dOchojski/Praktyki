2160c7953b1af511e046e0362406864ba8e6b9eb
Sebastian <sebasqbi@gmail.com>
2024-02-21 11:10:00 +0100

    Handle Elevator methods

--- src/main/java/com/d_d/praktyki/arrays/elevator/Elevator.java
+++ src/main/java/com/d_d/praktyki/arrays/elevator/Elevator.java
@@ -2,11 +2,34 @@ package com.d_d.praktyki.arrays.elevator;
 
 public class Elevator {
     private int max_people_amount;
+    private int weightOverload;
     private Person[] people = new Person[max_people_amount];
-    public Elevator() { this(4); }
-    public Elevator(int max_people) {
+    private int people_count = 0;
+    public Elevator() { this(4, 400); }
+    public Elevator(int max_people, int max_weight) {
         max_people_amount = max_people;
+        weightOverload = max_weight;
     }
-    public void add(Person person) {}
-    public void start() {}
+    public void add(Person person) {
+        if (people_count >= 4) {
+            System.out.println("Too many people, there is no space! Max.: "+max_people_amount);
+            return;
+        }
+        people[people_count] = person;
+        people_count++;
+    }
+    public void start() {
+        int totalWeight = 0;
+        for (int i=0; i<people_count; i++) {
+            totalWeight += people[i].getWeight();
+        }
+        if (totalWeight >= weightOverload) {
+            System.out.println(
+                "Elevator is overloaded! Total weight: "+totalWeight+" kg"
+                +", allowed: "+weightOverload
+            );
+            return;
+        }
+        System.out.println("Elevator going up!");
+    }
 }
