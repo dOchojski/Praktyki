8c63733cb8af6e7b8ed2bd0cd92b93b9cfd24d92
Sebastian <sebasqbi@gmail.com>
2024-02-21 10:46:09 +0100

    Add Elevator class

--- src/main/java/com/d_d/praktyki/arrays/elevator/Elevator.java
+++ src/main/java/com/d_d/praktyki/arrays/elevator/Elevator.java
@@ -1,5 +1,12 @@
 package com.d_d.praktyki.arrays.elevator;
 
 public class Elevator {
-
+    private int max_people_amount;
+//    private Person[] people = new Person[];
+    public Elevator() { this(4); }
+    public Elevator(int max_people) {
+        max_people_amount = max_people;
+    }
+    public void add(/*Person person*/) {}
+    public void start() {}
 }
--- src/main/java/com/d_d/praktyki/arrays/elevator/ElevatorTest.java
+++ src/main/java/com/d_d/praktyki/arrays/elevator/ElevatorTest.java
@@ -2,6 +2,25 @@ package com.d_d.praktyki.arrays.elevator;
 
 public class ElevatorTest {
     public static void main(String[] args) {
-        
+        Elevator tooHeavyTest = new Elevator();
+        tooHeavyTest.add();
+        tooHeavyTest.add();
+        tooHeavyTest.add();
+        tooHeavyTest.start();
+
+        Elevator normalTest = new Elevator();
+        normalTest.add();
+        normalTest.add();
+        normalTest.add();
+        normalTest.add();
+        normalTest.start();
+
+        Elevator tooManyPeopleTest = new Elevator();
+        tooManyPeopleTest.add();
+        tooManyPeopleTest.add();
+        tooManyPeopleTest.add();
+        tooManyPeopleTest.add();
+        tooManyPeopleTest.add();
+        normalTest.start();
     }
 }
