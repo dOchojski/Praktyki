cedd503a6c0d9b570c95db426a1d2c37056f5ff1
Sebastian <sebasqbi@gmail.com>
2024-02-21 10:35:27 +0100

    Define method

--- src/main/java/com/d_d/praktyki/arrays/onedimensional/OneDimensionalTest.java
+++ src/main/java/com/d_d/praktyki/arrays/onedimensional/OneDimensionalTest.java
@@ -4,12 +4,15 @@ public class OneDimensionalTest {
     public static void main(String[] args) {
         int[] a = {1,2,3};
         int[] b = {6,-2,7};
+        int sum = sum(a, b);
+        System.out.print("Suma wszystkich liczb obu metod: "+sum);
+    }
+
+    private static int sum(int[] a, int[] b) {
         int sum = 0;
-        // (samo IDE mi podpowiada żeby użyć .stream().sum() z java.util.Arrays)
-        for (int i=0; i<3; i++) {
+        for (int i=0; i<3; i++)
             // reduce all values to one sum
             sum += a[i]+b[i];
-        }
-        System.out.print("Suma wszystkich liczb obu metod: "+sum);
+        return sum;
     }
 }
