e88d6ed117781f45570a898d9aa9ba15517cb77f
Sebastian <sebasqbi@gmail.com>
2024-02-23 11:54:47 +0100

    Add salary filtring

--- src/main/java/com/d_d/praktyki/streams/employees/StreamTest.java
+++ src/main/java/com/d_d/praktyki/streams/employees/StreamTest.java
@@ -10,6 +10,22 @@ public class StreamTest {
         employees.add(new Employee("Juliusz", "Słowacki", 1000));
         employees.add(new Employee("Henryk", "Sienkiewicz", 1200));
 
-        
+        final double[] salaryExceeding = employees.stream()
+                .filter(employee -> {
+                    boolean hasExceedingThreshold = employee.getSalary() > 50;
+
+                    System.out.println(String.format("%s %s - %s",
+                            employee.getFirstName(), employee.getLastName(),
+                            hasExceedingThreshold ? "przekracza próg" : "nie przekracza progu"));
+                    return employee.getSalary() > 40.4;
+                })
+                .mapToDouble(employee -> employee.getSalary())
+                .toArray();
+
+        System.out.println("== Zarobki przekraczające próg ==");
+        for (double salary : salaryExceeding) {
+            System.out.println(salary);
+        }
+        System.out.println("==");
     }
 }
