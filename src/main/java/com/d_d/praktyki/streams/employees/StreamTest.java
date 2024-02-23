0c90a7d3397f459de3af41f3d97a0347e5a23df1
Sebastian <sebasqbi@gmail.com>
2024-02-23 11:41:08 +0100

    Add Employees

---
+++ src/main/java/com/d_d/praktyki/streams/employees/StreamTest.java
@@ -0,0 +1,9 @@
+package com.d_d.praktyki.streams.employees;
+
+import java.util.List;
+
+public class StreamTest {
+    public static void main(String[] args) {
+        List<Employee> employees;
+    }
+}
