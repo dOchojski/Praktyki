b1c04e02bf5f424d557a97f9cb6734c5b288fd9f
Sebastian <sebasqbi@gmail.com>
2024-02-23 11:44:58 +0100

    Add employees to list

--- src/main/java/com/d_d/praktyki/streams/employees/StreamTest.java
+++ src/main/java/com/d_d/praktyki/streams/employees/StreamTest.java
@@ -1,9 +1,15 @@
 package com.d_d.praktyki.streams.employees;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StreamTest {
     public static void main(String[] args) {
-        List<Employee> employees;
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee("Adam", "Mickiewicz", 40.4));
+        employees.add(new Employee("Juliusz", "Słowacki", 1000));
+        employees.add(new Employee("Henryk", "Sienkiewicz", 1200));
+
+        
     }
 }
