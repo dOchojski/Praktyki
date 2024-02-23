7d0119dfd3f9573d5bcc9a1cc636e73965947cce
Sebastian <sebasqbi@gmail.com>
2024-02-23 12:26:48 +0100

    Add animals to list

--- src/main/java/com/d_d/praktyki/streams/animals/StreamTest.java
+++ src/main/java/com/d_d/praktyki/streams/animals/StreamTest.java
@@ -1,7 +1,16 @@
 package com.d_d.praktyki.streams.animals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StreamTest {
     public static void main(String[] args) {
-        
+        Dog fafik = new Dog();
+        Cat malkolm = new Cat();
+        Whale antos = new Whale();
+        Fish nemo = new Fish();
+
+        List<Animal> animals = Arrays.asList(fafik, malkolm, antos, nemo);
     }
 }
