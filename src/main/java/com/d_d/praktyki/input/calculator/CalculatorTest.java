fa1d92d1d73955011ad704d649aff297702ab531
Sebastian <sebasqbi@gmail.com>
2024-02-28 10:24:23 +0100

    Add InputPrompt class

--- src/main/java/com/d_d/praktyki/input/calculator/CalculatorTest.java
+++ src/main/java/com/d_d/praktyki/input/calculator/CalculatorTest.java
@@ -1,7 +1,11 @@
 package com.d_d.praktyki.input.calculator;
 
+import java.util.Scanner;
+
 public class CalculatorTest {
     public static void main(String[] args) {
-        Calculator.prompt();
+//        Calculator.prompt();
+        InputPrompt input = new InputPrompt(new Scanner(System.in));
+        input.promptDouble("AAA", true);
     }
 }
