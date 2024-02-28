0af1cccb22f74fa5f112cd84d36906ae6fccd105
Sebastian <sebasqbi@gmail.com>
2024-02-28 11:33:45 +0100

    Add main loop

--- src/main/java/com/d_d/praktyki/loops/hospital/HospitalTest.java
+++ src/main/java/com/d_d/praktyki/loops/hospital/HospitalTest.java
@@ -1,7 +1,30 @@
 package com.d_d.praktyki.loops.hospital;
 
+import com.d_d.praktyki.input.InputPrompt;
+
+import java.util.Scanner;
+
 public class HospitalTest {
+    private static final InputPrompt inputPrompt = new InputPrompt(new Scanner(System.in));
+
     public static void main(String[] args) {
-        
+        boolean isRunning = true;
+
+        while (isRunning) {
+            String option = inputPrompt.prompt(
+                    "0 - wyjście z programu" + "\n" +
+                    "1 - dopisywanie pacjenta" + "\n" +
+                    "2 - wyświetlanie zapisanych pacjentów");
+
+            switch (option) {
+                case "0":
+                    isRunning = false;
+                    break;
+                case "1":
+                    break;
+                case "2":
+                    break;
+            }
+        }
     }
 }
