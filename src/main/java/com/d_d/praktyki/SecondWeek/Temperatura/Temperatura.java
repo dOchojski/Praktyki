e1669d1fe5e357f79a5de720909ea341e93340cc
Holmynik <dominikkijko4@gmail.com>
2024-03-01 12:03:47 +0100

    Temperatura

--- src/main/java/com/d_d/praktyki/SecondWeek/Temperatura/Temperatura.java
+++ src/main/java/com/d_d/praktyki/SecondWeek/Temperatura/Temperatura.java
@@ -1,2 +1,17 @@
-package com.d_d.praktyki.SecondWeek.Temperatura;public class Temperatura {
+package com.d_d.praktyki.SecondWeek.Temperatura;
+
+public class Temperatura {
+
+    public static void regulateTemperature(double currentTemp, int targetTemp) {
+        while (currentTemp != targetTemp) {
+            if (currentTemp < targetTemp) {
+                currentTemp += 0.5;
+                System.out.println("Aktualna temperatura: " + currentTemp + "°C");
+            } else if (currentTemp > targetTemp) {
+                currentTemp -= 0.5;
+                System.out.println("Aktualna temperatura: " + currentTemp + "°C");
+            }
+        }
+        System.out.println("Temperatura docelowa osiągnięta.");
+    }
 }
