dae009158dfcf458b70e97da67fd907dc31a158d
Sebastian <sebasqbi@gmail.com>
2024-03-05 08:33:53 +0100

    Add tests

--- src/main/java/com/d_d/praktyki/inheritance/courses/CoursesTest.java
+++ src/main/java/com/d_d/praktyki/inheritance/courses/CoursesTest.java
@@ -2,6 +2,22 @@ package com.d_d.praktyki.inheritance.courses;
 
 public class CoursesTest {
     public static void main(String[] args) {
-
+        OnlineCourse cppCourse = new OnlineCourse("CPP-ZELENT",
+                "C++ Course and how to make timer only for Windows :P",
+                "This course will allow to make only platform-dependent (Windows) timer in " +
+                        "old WinApi Builder",
+                1000,
+                45,
+                60
+        );
+        BootcampCourse bootstrapCourse = new BootcampCourse("BOOTSTRAP-1125125124",
+                "Bootstrap Bootcamp Course",
+                "Bootstrap Bootcamp Course about Bootstrap",
+                12.25,
+                30,
+                50,
+                "Adam Nowak",
+                3
+        );
     }
 }
